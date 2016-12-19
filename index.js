36941a03691323a38804f8e8a4ca27663c2d36cf
Chris Tozer <christopherjtozer@gmail.com>
2016-12-19 14:47:19 +0000

    Add repo data to defaults.yaml

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var program = require('commander');
 var chalk = require('chalk');
 var Config = require('merge-config');
 
+// Main entrypoint using commander - calls build_map
+program
+  .arguments('<spec_file>', 'Config file defining the map to build.  E.g. examples/france.yaml')
+  .option('-t, --test', 'Copy resulting map to test server')
+  .action(build_map)
+  .parse(process.argv);
+
+// Main processing function
 function build_map(spec_file) {
   var config = new Config();
   config.file('defaults.yaml');
@@ -13,8 +21,7 @@ function build_map(spec_file) {
   console.log(chalk.bold.green('Complete!') + '  Finished processing for ' + spec_file);
 }
 
-program
-  .arguments('<spec_file>', 'Config file defining the map to build.  E.g. examples/france.yaml')
-  .option('-t, --test', 'Copy resulting map to test server')
-  .action(build_map)
-  .parse(process.argv);
+// Ensure raw data is available
+function ensure_data(config) {
+  // @@@ TODO
+}
