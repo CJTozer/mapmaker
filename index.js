5a56eed6b53042b2fce1bf9177924c26c607435d
Chris Tozer <christopherjtozer@gmail.com>
2016-12-19 14:41:40 +0000

    Refactor map building into its own function, separate from commander

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ var program = require('commander');
 var chalk = require('chalk');
 var Config = require('merge-config');
 
-program
-  .arguments('<spec_file>')
-  .option('-t, --test', 'Copy resulting map to test server')
-  .action(function(spec_file) {
-    var config = new Config();
-    config.file('defaults.yaml');
-    config.file(spec_file);
-    console.log(chalk.bold.cyan('Config:'));
-    console.log(config.get());
-    console.log(chalk.bold.green('Complete!') + '  Finished processing for ' + spec_file);
-  })
-  .parse(process.argv);
+function build_map(spec_file) {
+  var config = new Config();
+  config.file('defaults.yaml');
+  config.file(spec_file);
 
+  console.log(chalk.bold.cyan('Config:'));
+  console.log(config.get());
+  console.log(chalk.bold.green('Complete!') + '  Finished processing for ' + spec_file);
+}
+
+program
+  .arguments('<spec_file>', 'Config file defining the map to build.  E.g. examples/france.yaml')
+  .option('-t, --test', 'Copy resulting map to test server')
+  .action(build_map)
+  .parse(process.argv);
