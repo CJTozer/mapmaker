741834585c85ed54274ab17673fe195b64bf7ef7
CJTozer <christopherjtozer@gmail.com>
2016-12-19 00:07:31 +0000

    More countries in Europe

--- examples/europe.mk
+++ examples/europe.mk
@@ -7,7 +7,7 @@ examples_europe_DATA_DIR = $(examples_europe_BASE_DATA)/$(examples_europe_DATA_T
 examples_europe_DATA_SHP = $(examples_europe_DATA_DIR)/$(examples_europe_DATA_TYPE).shp
 
 # Behaviour
-examples_europe_FILTER = "ADM0_A3 IN ('FRA', 'GBR')"
+examples_europe_FILTER = "ADM0_A3 IN ('FRA', 'GBR', 'ITA', 'ESP', 'DNK', 'BEL', 'DEU', 'NLX', 'LUX', 'CHE', 'AUT', 'LIE', 'AND')"
 examples_europe_FILTER_SHA = $(call get_sha,$(examples_europe_DATA_DIR) $(FILTER))
 examples_europe_INTERMEDIATE_GEO = $(BUILD_DIR)/$(examples_europe_FILTER_SHA).geo.json
 examples_europe_INTERMEDIATE_TOPO = $(BUILD_DIR)/$(examples_europe_FILTER_SHA).topo.json
@@ -19,6 +19,7 @@ examples/europe: build-dir
 	# Make intermediate GeoJSON file, with specified filter
 	rm -f $(examples_europe_INTERMEDIATE_GEO)
 	ogr2ogr -f GeoJSON -where $(examples_europe_FILTER) $(examples_europe_INTERMEDIATE_GEO) $(examples_europe_DATA_SHP)
+	# ogr2ogr -f GeoJSON $(examples_europe_INTERMEDIATE_GEO) $(examples_europe_DATA_SHP)
 	# Make intermediate TopoJSON file (give it a sensible name in the mean time though as this is used in the JSON)
 	cp $(examples_europe_INTERMEDIATE_GEO) $(BUILD_DIR)/subunits.json
 	topojson -o $(examples_europe_INTERMEDIATE_TOPO) $(examples_europe_TOPO_FLAGS) -- $(BUILD_DIR)/subunits.json
