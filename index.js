51ee72be11104ca92872af5a8a949e3b1b9790dc
Chris Tozer <christopherjtozer@gmail.com>
2016-12-19 10:50:15 +0000

    Arguments parsing

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-console.log('mapmaker ...');
+var program = require('commander');
+
+program
+  .arguments('<spec_file>')
+  .option('-t, --test', 'Copy resulting map to test server')
+  .action(function(spec_file) {
+    console.log('spec_file: %s, test: %s', spec_file, program.test);
+  })
+  .parse(process.argv);
