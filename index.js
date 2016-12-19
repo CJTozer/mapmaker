9b3be905fb4af76407d094a9b2c2e4b549c3281d
Chris Tozer <christopherjtozer@gmail.com>
2016-12-19 10:52:16 +0000

    Colours using chalk

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 var program = require('commander');
+var chalk = require('chalk');
 
 program
   .arguments('<spec_file>')
   .option('-t, --test', 'Copy resulting map to test server')
   .action(function(spec_file) {
     console.log('spec_file: %s, test: %s', spec_file, program.test);
+    console.log(chalk.bold.green('Complete!') + '  Finished processing for ' + spec_file);
   })
   .parse(process.argv);
