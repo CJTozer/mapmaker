86eabf37175387ab52afe89d56ee1449fe003dc3
Chris Tozer <christopherjtozer@gmail.com>
2016-12-27 18:35:44 +0000

    Fix up get_projection, return correct error name

--- libs/projections/index.js
+++ libs/projections/index.js
@@ -9,14 +9,22 @@ var Projections = function () {};
 
 // Get the projection object from the specified config.
 Projections.prototype.get_projection = function (config) {
-  var err = null;
-  var projection = d3.geoMercator()
-    .center(config.parameters.projection.center)
-    .scale(config.parameters.projection.scale)
-    .translate([
-      config.parameters.projection.width / 2,
-      config.parameters.projection.height / 2]);
-  return {err, projection};
+  var proj_err = null;
+  var projection = null;
+  var proj_type = config.parameters.projection.type.toLowerCase();
+  switch (proj_type) {
+    case "mercator":
+      projection = d3.geoMercator()
+        .center(config.parameters.projection.center)
+        .scale(config.parameters.projection.scale)
+        .translate([
+          config.parameters.projection.width / 2,
+          config.parameters.projection.height / 2]);
+      break;
+    default:
+      proj_err = `Unrecognized projection "${proj_type}""`;
+  }
+  return {proj_err, projection};
 };
 
 // Finally, export the object.
