92723cfa37eaff044f4e5767774f3b0681ae1002
CJTozer <christopherjtozer@gmail.com>
2017-01-03 20:02:14 +0000

    Projections doesn't actually need a class

--- libs/projections/index.js
+++ libs/projections/index.js
@@ -5,27 +5,26 @@
 
 const d3 = require( 'd3' );
 
-class Projections {
-  // Get the projection object from the specified config.
-  get_projection( config ) {
-    var
-      proj_err = null,
-      projection = null,
-      proj_type = config.parameters.projection.type.toLowerCase();
-    switch ( proj_type ) {
-    case 'mercator':
-      projection = d3.geoMercator();
-      break;
-    case 'albers':
-      // @@@ Get parallels from config?
-      projection = d3.geoAlbers()
+// Get the projection object from the specified config.
+function get_projection( config ) {
+  var
+    proj_err = null,
+    projection = null,
+    proj_type = config.parameters.projection.type.toLowerCase();
+  switch ( proj_type ) {
+  case 'mercator':
+    projection = d3.geoMercator();
+    break;
+  case 'albers':
+    // @@@ Get parallels from config?
+    projection = d3.geoAlbers()
       .parallels( [ 50, 60 ] );
-      break;
-    default:
-      proj_err = `Unrecognized projection "${proj_type}""`;
-    }
-    if ( !proj_err ) {
-      projection = projection
+    break;
+  default:
+    proj_err = `Unrecognized projection "${proj_type}""`;
+  }
+  if ( !proj_err ) {
+    projection = projection
       .center( config.parameters.projection.center )
       .scale( config.parameters.projection.scale )
       .translate( [
@@ -33,13 +32,11 @@ class Projections {
         config.parameters.projection.width / 2
       ] )
       .rotate( config.parameters.projection.rotation );
-    }
-    return {
-      proj_err,
-      projection
-    };
   }
+  return {
+    proj_err,
+    projection
+  };
 }
 
-// Finally, export the object.
-module.exports = new Projections();
+module.exports.get_projection = get_projection;
