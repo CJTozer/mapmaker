7fd5911232449ab55e86eca5afdbd347f725a409
CJTozer <christopherjtozer@gmail.com>
2017-01-03 20:20:17 +0000

    Gruntfile starter for 10 - eslint

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+/* jshint esversion: 6 */
+'use strict';
+
+module.exports = function( grunt ) {
+  grunt.initConfig( {
+    eslint: {
+      options: {
+        configFile: '.eslintrc.yaml',
+        fix: true,
+      },
+      src: [ '.' ],
+    },
+  } );
+
+  grunt.loadNpmTasks( 'gruntify-eslint' );
+  grunt.registerTask( 'default', [ 'lint' ] );
+  grunt.registerTask( 'lint', [ 'eslint' ] );
+};
