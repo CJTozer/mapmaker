59feb9c03b1aacc299a8ac9258de33a42b3193e0
CJTozer <christopherjtozer@gmail.com>
2017-01-03 19:13:47 +0000

    eslint with idiomatic settings - autofixes

--- libs/projections/index.js
+++ libs/projections/index.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
 
 /* jshint esversion: 6 */
-"use strict";
+'use strict';
 
-const d3 = require("d3");
+const d3 = require( 'd3' );
 
 // Anonymous object representing the module.
 var Projections = function() {};
 
 // Get the projection object from the specified config.
-Projections.prototype.get_projection = function(config) {
+Projections.prototype.get_projection = function( config ) {
   var proj_err = null;
   var projection = null;
   var proj_type = config.parameters.projection.type.toLowerCase();
-  switch (proj_type) {
-    case "mercator":
-      projection = d3.geoMercator();
-      break;
-    case "albers":
+  switch ( proj_type ) {
+  case 'mercator':
+    projection = d3.geoMercator();
+    break;
+  case 'albers':
       // @@@ Get these from config?
-      projection = d3.geoAlbers()
-        .parallels([50, 60]);
-      break;
-    default:
-      proj_err = `Unrecognized projection "${proj_type}""`;
+    projection = d3.geoAlbers()
+        .parallels( [ 50, 60 ] );
+    break;
+  default:
+    proj_err = `Unrecognized projection "${proj_type}""`;
   }
-  if (!proj_err) {
+  if ( !proj_err ) {
     projection = projection
-      .center(config.parameters.projection.center)
-      .scale(config.parameters.projection.scale)
-      .translate([
+      .center( config.parameters.projection.center )
+      .scale( config.parameters.projection.scale )
+      .translate( [
         config.parameters.projection.width / 2,
         config.parameters.projection.width / 2
-      ])
-      .rotate(config.parameters.projection.rotation);
+      ] )
+      .rotate( config.parameters.projection.rotation );
   }
   return {
     proj_err,
--- test/examples.js
+++ test/examples.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
 /* jshint esversion: 6 */
-"use strict";
+'use strict';
 
-const chai = require('chai');
+const chai = require( 'chai' );
 const assert = chai.assert;
-const fs = require('fs');
-const path = require("path");
+const fs = require( 'fs' );
+const path = require( 'path' );
 
-const MapMaker = require('../index.js');
+const MapMaker = require( '../index.js' );
 
-describe('ExampleGeneration', function() {
+describe( 'ExampleGeneration', function() {
   // Increase timeout - when running on a fresh system, downloads are required.
-  this.timeout(180000);
-  var files = fs.readdirSync('./examples');
-  files.forEach(function(file) {
-    it(`Generate example map from ${file}`, function(done) {
-      var spec_file = path.join('./examples', file);
+  this.timeout( 180000 );
+  var files = fs.readdirSync( './examples' );
+  files.forEach( function( file ) {
+    it( `Generate example map from ${file}`, function( done ) {
+      var spec_file = path.join( './examples', file );
       new MapMaker()
-        .specFile(spec_file)
-        .onError((err) => {
-          done(err);
-        })
-        .onSuccess((data) => {
+        .specFile( spec_file )
+        .onError( ( err ) => {
+          done( err );
+        } )
+        .onSuccess( ( data ) => {
           // assert.equal("+++", "---");
           done();
-        })
+        } )
         .build_map();
-    });
-  });
-});
+    } );
+  } );
+} );
