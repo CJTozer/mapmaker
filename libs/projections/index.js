51a0c11632757eacae9917a05459d0f3c0828f5b
CJTozer <christopherjtozer@gmail.com>
2017-01-03 20:00:00 +0000

    Update class syntax for Projections

--- libs/projections/index.js
+++ libs/projections/index.js
@@ -5,29 +5,27 @@
 
 const d3 = require( 'd3' );
 
-// Anonymous object representing the module.
-var Projections = function() {};
-
-// Get the projection object from the specified config.
-Projections.prototype.get_projection = function( config ) {
-  var
-    proj_err = null,
-    projection = null,
-    proj_type = config.parameters.projection.type.toLowerCase();
-  switch ( proj_type ) {
-  case 'mercator':
-    projection = d3.geoMercator();
-    break;
-  case 'albers':
-      // @@@ Get these from config?
-    projection = d3.geoAlbers()
-        .parallels( [ 50, 60 ] );
-    break;
-  default:
-    proj_err = `Unrecognized projection "${proj_type}""`;
-  }
-  if ( !proj_err ) {
-    projection = projection
+class Projections {
+  // Get the projection object from the specified config.
+  get_projection( config ) {
+    var
+      proj_err = null,
+      projection = null,
+      proj_type = config.parameters.projection.type.toLowerCase();
+    switch ( proj_type ) {
+    case 'mercator':
+      projection = d3.geoMercator();
+      break;
+    case 'albers':
+      // @@@ Get parallels from config?
+      projection = d3.geoAlbers()
+      .parallels( [ 50, 60 ] );
+      break;
+    default:
+      proj_err = `Unrecognized projection "${proj_type}""`;
+    }
+    if ( !proj_err ) {
+      projection = projection
       .center( config.parameters.projection.center )
       .scale( config.parameters.projection.scale )
       .translate( [
@@ -35,12 +33,13 @@ Projections.prototype.get_projection = function( config ) {
         config.parameters.projection.width / 2
       ] )
       .rotate( config.parameters.projection.rotation );
+    }
+    return {
+      proj_err,
+      projection
+    };
   }
-  return {
-    proj_err,
-    projection
-  };
-};
+}
 
 // Finally, export the object.
 module.exports = new Projections();
